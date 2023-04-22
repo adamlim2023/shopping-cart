875d5509c13beed11233da25130d050770bdc002
adamlim2023 <adamlim2023@gmail.com>
2023-04-21 20:19:11 -0700

    fix

--- src/components/Common/AddButton.jsx
+++ src/components/Common/AddButton.jsx
@@ -3,7 +3,10 @@ import PlusIcon from "assets/images/icons/plus.svg";
 
 const AddButton = ({ onClick }) => {
   return (
-    <button className="h-8 pl-1 pr-2 flex items-center shadow-button-add rounded-[10px] bg-[#F0F0F3]" onClick={onClick}>
+    <button
+      className="h-8 pl-1 pr-2 flex items-center shadow-button-add rounded-[10px] bg-[#F0F0F3] transition hover:opacity-70"
+      onClick={onClick}
+    >
       <img src={PlusIcon} className="mr-2" alt="add item" />
       <span className="font-medium text-[13px] leading-[22px]">ADD</span>
     </button>
